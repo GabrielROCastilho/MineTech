1fb1db9ea945ab7128c8b2c47b1a3d4ae2a7a15a
pedrorico79 <pedro.rico@sptech.school>
2025-04-28 20:06:17 -0300

    adaptação select BD

--- Banco de Dados/BD PI - select.sql
+++ Banco de Dados/BD PI - select.sql
@@ -1,30 +1,32 @@
-select 	m.nome, 
+select 	m.nome nome_empresa, 
 		m.cnpj,
         e.cep,
-        c.nome,
-        uf.nome
+        c.nome cidade,
+        uf.nome estado
 from mineradora as m
 inner join endereco as e on e.id = m.fkEndereco
 inner join cidade as c on c.id = e.fkCidade
 inner join estado as uf on uf.id = c.fkEstado;
 
 
-select	f.nome,
+select	f.nome nome,
 		f.sobrenome,
         f.telefone_celular,
-        c.nome
+        c.nome cargo
 from funcionario as f
 inner join cargo as c on c.id=f.fkcargo;
 
 
-select	s.id,
-		s.statussensor,
-        m.nivelmetano,
-        m.datahora,
-        l.localsensor,
-        n.statusnivel
+select	s.id id_setor,
+		s.statussensor status_sensor,
+        m.nivelmetano nivel_metano,
+        m.datahora hora_medicao,
+        n.statusnivel status_nivel,
+        l.localsensor local_sensor,
+        se.sigla sigla_setor
 from sensor as s
-inner join localsensor as l on l.id=s.fkLocal
+inner join localSensor as l on l.id=s.fkLocal
 inner join medicao as m on m.id = s.fkMedicao
-inner join nivelgas as n on n.id = s.fkNivelGas;
+inner join nivelGas as n on n.id = s.fkNivelGas
+inner join setor as se on se.idsetor = l.fksetor;
         
